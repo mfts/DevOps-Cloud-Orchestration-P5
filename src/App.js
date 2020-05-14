10c5d8b9a4caba6c594cbfc2e2453c3a8f8f32a2
Marc Seitz <marc@mfts.io>
2020-05-15 00:31:30 +0300

    add jitsi client files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import Jutsu from './Jutsu'
+
+const App = () => {
+  const [room, setRoom] = useState('')
+  const [name, setName] = useState('')
+  const [call, setCall] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const handleClick = event => {
+    event.preventDefault()
+    if (room && name) setCall(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Jitsi Meet Client</h2>
+      {call ? (<Jutsu
+        roomName={room}
+        userName={name}
+        password={password}
+        loadingComponent={<p>ʕ •ᴥ•ʔ jitsi is loading ...</p>} />)
+        : (
+          <form>
+            <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+            <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+            <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleClick} type='submit'>
+              Start / Join
+            </button>
+          </form>
+        )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Jutsu.js
@@ -0,0 +1,76 @@
+import React, { createRef, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+const Jutsu = ({
+  roomName,
+  userName,
+  domain = 'meet.jit.si',
+  password,
+  subject,
+  loadingComponent,
+  containerStyles,
+  jitsiContainerStyles
+}) => {
+  const [loading, setLoading] = useState(true)
+  const ref = createRef()
+
+  const containerStyle = {
+    width: '800px',
+    height: '400px'
+  }
+
+  const jitsiContainerStyle = {
+    display: loading ? 'none' : 'block',
+    width: '100%',
+    height: '100%'
+  }
+
+  const startConference = () => {
+    try {
+      // eslint-disable-next-line no-undef
+      const api = new JitsiMeetExternalAPI(domain, { roomName, parentNode: ref.current })
+      if (subject) {
+        api.executeCommand('subject', subject)
+      }
+      api.addEventListener('videoConferenceJoined', () => {
+        console.info(`${userName} has entered ${roomName}`)
+        setLoading(false)
+        api.executeCommand('displayName', userName)
+        if (password) {
+          api.executeCommand('password', password)
+        }
+      })
+    } catch (error) {
+      console.error('Failed to load Jitsi API', error)
+    }
+  }
+
+  useEffect(() => {
+    if (window.JitsiMeetExternalAPI) startConference()
+    else console.error('Jitsi Meet API script not loaded')
+  }, [])
+
+  return (
+    <div style={{ ...containerStyle, ...containerStyles }}>
+      {loading && (loadingComponent || <p>Loading ...</p>)}
+      <div
+        id="jitsi-container"
+        ref={ref}
+        style={{ ...jitsiContainerStyle, ...jitsiContainerStyles }}
+      />
+    </div>
+  )
+}
+
+Jutsu.propTypes = {
+  roomName: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+  domain: PropTypes.string,
+  password: PropTypes.string,
+  subject: PropTypes.string,
+  loadingComponent: PropTypes.object,
+  containerStyles: PropTypes.object,
+  jitsiContainerStyles: PropTypes.object
+}
+
+export default Jutsu
